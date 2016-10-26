724b670b9de3db1ba09c2c808f5c9f875f517493
Simon Wright <smw@ceh.ac.uk>
2016-10-26 13:36:51 +0100

    Adding update-arcgis-pro-project.py to repository

---
+++ update-arcgis-pro-project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+__author__ = "smw"
+__email__ = "smw@ceh.ac.uk"
+
+
+import os
+import sys
+import time
+
+
+import arcpy
+
+
+print('\n\n{}'.format(sys.version))
+
+
+print('Cheese')
+print('Sleeping...')
+time.sleep(5)
+print('Slept.')
+
+
+print('Done.')
