5935e49e69bf9c931383b711040f6c4ea9328afe
Simon Wright <smw@ceh.ac.uk>
2016-10-27 13:17:22 +0100

    Added map frame elements - north arrow, scale bar, and legend - to lay out.

--- update-arcgis-pro-project.py
+++ update-arcgis-pro-project.py
@@ -84,42 +84,46 @@ for layout in layouts:
     print('\tlayout.pageHeight:\t\t{0}'.format(layout.pageHeight))
     print('\tlayout.pageWidth:\t\t{0}'.format(layout.pageWidth))
     print('\tlayout.pageUnits:\t\t{0}'.format(layout.pageUnits))
-    for element in layout.listElements("MAPFRAME_ELEMENT"):
-        print('\t\telement.name:\t\t\t\t{0}'.format(element.name))
+    for element in layout.listElements(element_type='',
+                                       wildcard=None):
+        print('\t\t#\n\t\telement.name:\t\t\t\t{0}'.format(element.name))
         print('\t\telement.type:\t\t\t\t{0}'.format(element.type))
-        print('\t\telement.elementHeight:\t\t{0}'.format(element.elementHeight))
-        print('\t\telement.elementWidth:\t\t{0}'.format(element.elementWidth))
-        print('\t\telement.elementPositionX:\t{0}'.format(element.elementPositionX))
-        print('\t\telement.elementPositionY:\t{0}'.format(element.elementPositionY))
-        print('\t\telement.elementRotation:\t{0}'.format(element.elementRotation))
-        print('\t\telement.visble:\t\t\t\t{0}'.format(element.visible))
+    for mapframe in layout.listElements(element_type='MAPFRAME_ELEMENT',
+                                        wildcard=None):
+        print('\t\t#\n\t\tmapframe.name:\t\t\t\t{0}'.format(mapframe.name))
+        print('\t\tmapframe.type:\t\t\t\t{0}'.format(mapframe.type))
+        print('\t\tmapframe.elementHeight:\t\t{0}'.format(mapframe.elementHeight))
+        print('\t\tmapframe.elementWidth:\t\t{0}'.format(mapframe.elementWidth))
+        print('\t\tmapframe.elementPositionX:\t{0}'.format(mapframe.elementPositionX))
+        print('\t\tmapframe.elementPositionY:\t{0}'.format(mapframe.elementPositionY))
+        print('\t\tmapframe.elementRotation:\t{0}'.format(mapframe.elementRotation))
+        print('\t\tmapframe.visble:\t\t\t\t{0}'.format(mapframe.visible))
         #
-        camera = element.camera
-        print('\t\tcaemra.mode:\t\t{0}'.format(camera.mode))
-        print('\t\tcaemra.scale:\t\t{0}'.format(camera.scale))
-        print('\t\tcaemra.X:\t\t\t{0}'.format(camera.X))
-        print('\t\tcaemra.Y:\t\t\t{0}'.format(camera.Y))
-        print('\t\tcamera.getExtent():\t{0}'.format(camera.getExtent()))
+        print('\t\tmapframe.camera.mode:\t\t{0}'.format(mapframe.camera.mode))
+        print('\t\tmapframe.camera.scale:\t\t{0}'.format(mapframe.camera.scale))
+        print('\t\tmapframe.camera.X:\t\t\t{0}'.format(mapframe.camera.X))
+        print('\t\tmapframe.camera.Y:\t\t\t{0}'.format(mapframe.camera.Y))
+        print('\t\tmapframe.camera.getExtent():\t{0}'.format(mapframe.camera.getExtent()))
 
         # Change extent using camera setExtent() method
         # new_extent = arcpy.Extent(0.0, 0.0, 700000.0, 1300000.0)
-        # element.camera.setExtent(new_extent)        
+        # mapframe.camera.setExtent(new_extent)        
         # Alternatively, change extent by updating camera X and position and scale
-        element.camera.X = 342500
-        element.camera.Y = 457500
-        element.camera.scale = 10000
-        print('\t\telement.camera.mode:\t\t{0}'.format(element.camera.mode))
-        print('\t\telement.camera.scale:\t\t{0}'.format(element.camera.scale))
-        print('\t\telement.camera.X:\t\t\t{0}'.format(element.camera.X))
-        print('\t\telement.camera.Y:\t\t\t{0}'.format(element.camera.Y))
-        print('\t\telement.camera.getExtent():\t{0}'.format(element.camera.getExtent()))
+        mapframe.camera.X = 342500
+        mapframe.camera.Y = 457500
+        mapframe.camera.scale = 10000
+        print('\t\tmapframe.camera.mode:\t\t{0}'.format(mapframe.camera.mode))
+        print('\t\tmapframe.camera.scale:\t\t{0}'.format(mapframe.camera.scale))
+        print('\t\tmapframe.camera.X:\t\t\t{0}'.format(mapframe.camera.X))
+        print('\t\tmapframe.camera.Y:\t\t\t{0}'.format(mapframe.camera.Y))
+        print('\t\tmapframe.camera.getExtent():\t{0}'.format(mapframe.camera.getExtent()))
         # Or use bookmarks (if they've been set) and the map frame element zoomToBookMark() method 
-        bookmarks = element.map.listBookmarks()
+        bookmarks = mapframe.map.listBookmarks()
         for bookmark in bookmarks:
             print('\t\tbookmark.map.name:\t\t{0}'.format(bookmark.map.name))
             print('\t\tbookmark.hasThumbnail:\t{0}'.format(bookmark.hasThumbnail))
             print('\t\tbookmark.name:\t\t\t{0}'.format(bookmark.name))
-            element.zoomToBookmark(bookmark)
+            mapframe.zoomToBookmark(bookmark)
             pdf = os.path.join(r'E:\CountrysideSurvey\cs2007-wgem-combined-schema\update-arcgis-pro-project', bookmark.name + r'.pdf')
             print('\t\tExporting to PDF {0}...'.format(pdf))
             layout.exportToPDF(out_pdf=pdf,
@@ -133,8 +137,6 @@ for layout in layouts:
                                jpeg_compression_quality=100,
                                clip_to_elements=False)
             print('\t\tExported to PDF {0}.'.format(pdf))
-
-
 
 
 print('\n\nSaving a copy of the ArcGIS Pro project...')
